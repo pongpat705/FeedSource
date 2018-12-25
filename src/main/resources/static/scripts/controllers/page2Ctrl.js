a217d655360982ce006f66cf2f9da3f2b5d806a8
FrankPR <surachate.frank@gmail.com>
2018-12-25 13:54:42 +0700

    datatable

--- src/main/resources/static/scripts/controllers/page2Ctrl.js
+++ src/main/resources/static/scripts/controllers/page2Ctrl.js
@@ -4,11 +4,36 @@ angular
     .module('app')
         .controller('page2Ctrl', [  '$scope', '$http', 'services',
   function page2Ctrl($scope, $http, services) {
-    
-     $scope.test = "test2";
+
     $scope.$watch("init", function(){
         
         console.log('page2');
+
+
+        $(document).ready(function() {
+           var table =  $('#news').DataTable( {
+                "aLengthMenu": [[20, 50, 75, -1], [20, 50, 75, "All"]],
+                "iDisplayLength": 20,
+                "processing": true,
+                "serverSide": true,
+                "ajax": {
+                    "url": "http://localhost:8080/api/getTitleList",
+                    "type": "GET"
+                },
+                "columns": [
+                     { "data": "title" },
+                    { "data": "url" },
+                    {"defaultContent": "<button class='btn btn-danger btn-xs'>X</button>"}
+                ]
+            } );
+
+            $('#news tbody').on( 'click', 'button', function () {
+                var data = table.row( $(this).parents('tr') ).data();
+                console.log( data.sourceId );
+            } );
+
+        } );
+
         
     });
     
